fe47d051c97691ab385dcece3f84e1013f7cbd3d
2017alexisperez <2017alexisperez@mac-15001220.local>
2017-02-24 09:21:30 -0800

    Initial Commit

---
+++ High School Application/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  High School Application
+//
+//  Created by 2017alexisperez on 2/24/17.
+//  Copyright © 2017 2017alexisperez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
